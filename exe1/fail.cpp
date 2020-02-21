f0f1924e2cbcba268f510f3ca663bee9858a2b38
Darren J Earl <darren.earl@ubtrobot.com>
2020-02-21 15:06:21 -0800

    run unit tests after building test, unfortunately deletes test if it fails, also need to figure out how to move files to the build dir robustly

--- exe1/fail.cpp
+++ exe1/fail.cpp
@@ -1,7 +1,13 @@
 #include <iostream>
+#include <cassert>
+#include <cstring>
+#include <cstdio>
 using namespace std;
 
-int main(){
+int main(int argc, char** argv){
 		cout << "hello world" << endl;
+		if( argc == 2 && strcmp( argv[1], "fail" ) == 0 ){
+				assert(0);
+		}
 		return 0;
 }
